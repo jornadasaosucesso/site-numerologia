e85af2ae841db51af17a0c44ed2970c831cd94ed
Jorge Barbosa Viana <jorgeviana2008@hotmail.com>
2025-11-07 09:27:33 -0400

    Correcao final do package.json e projeto limpo para Railway.

--- teste_tesseract.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-import os
-
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-os.environ["TESSDATA_PREFIX"] = r"C:\Program Files\Tesseract-OCR"
-
-print("Línguas disponíveis:", pytesseract.get_languages(config=''))
